04a0bfa5526ec7230676e258c70cb65ffa67d359
Yurchenko Illia <iliayurchenko06@gmail.com>
2025-02-16 14:39:57 +0200

    Added some explaining comments

--- server.go
+++ server.go
@@ -6,10 +6,12 @@ import (
     "time"
 )
 
+// Структура для JSON-відповіді
 type TimeResponse struct {
     Time string `json:"time"`
 }
 
+// Обробник HTTP-запиту для отримання поточного часу
 func timeHandler(w http.ResponseWriter, r *http.Request) {
     response := TimeResponse{Time: time.Now().Format(time.RFC3339)}
     w.Header().Set("Content-Type", "application/json")
